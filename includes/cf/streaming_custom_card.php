1353cd823c789eb1636a51d608539f7b2a655726
jerexxypunto_gear <jerexxypunto@gmail.com>
2022-11-03 08:32:38 -0300

    escondo streaming card si esta vacia

--- includes/cf/streaming_custom_card.php
+++ includes/cf/streaming_custom_card.php
@@ -34,3 +34,18 @@
         </div>
     </div>
 </div>
+
+<script>
+    const stream_isset = document.querySelectorAll('.twchr_card_body--list ul li span.value');
+    const stream_isset_array = [];
+    for (let i = 0; i < stream_isset.length; i++) {
+        const element = stream_isset[i];
+        //console.log(element.value);
+        if(element.value == undefined){
+            stream_isset_array.push(true);
+        }        
+    }
+    if(stream_isset_array.every(item => item === true)){
+        document.querySelector('.twchr_custom_card--contain').style.display = 'none';
+    }
+</script>
