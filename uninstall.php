023b180fe6920a2125bc30ff0175ee7ed1e14b04
jerexxypunto_gear <jerexxypunto@gmail.com>
2022-11-04 15:20:08 -0300

    eliminar todas las variables

--- uninstall.php
+++ uninstall.php
@@ -8,6 +8,7 @@ if (get_option('twchr_delete_all') == 1){
     delete_option('twchr_app_token');
     delete_option('twchr_setInstaled');
     delete_option('twchr_data_broadcaster');
+    delete_option('twchr_delete_all');
 
     $allposts= get_posts( array('post_type'=>'twchr_streams','numberposts'=>-1) );
     foreach ($allposts as $eachpost) {
