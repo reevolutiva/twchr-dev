b3120f89008c11f654221438d6f8a1c67b9962ce
jerexxypunto_gear <jerexxypunto@gmail.com>
2022-11-15 15:45:45 -0300

    bugfix de twitchtv

--- includes/api-connection/twichtv.php
+++ includes/api-connection/twichtv.php
@@ -1,11 +1,35 @@
 <?php
+/*
+ * TwitchTV API code by Elias Ranz-Schleifer
+ * Thank you for using my code please refer to
+ * https://github.com/Xxplosions/twitchtv-oauth for future updates
+ * Have questions or feedback? Contact Elias on Twitter (https://twitter.com/xxplosions)
+ * Check out my livestream at http://twitch.tv/xxplosions (It would be amazing to chat with you about future updates)
+    /: WordPress comes with an extensive HTTP API that should be used instead of creating your own curl calls. It’s both faster and more extensive. It’ll fall back to curl if it has to, but it’ll use a lot of WordPress’ native functionality first.
+ */
+ 
 class TwitchTV {
     var $base_url = "https://api.twitch.tv/kraken/";
     var $client_id = '';
     var $client_secret = '';
     var $redirect_url = '';
     var $scope_array = array('user_read','channel_read','chat_login','user_follows_edit','channel_editor','channel_commercial','channel_check_subscription');
- 
+      
+      /**
+       * Channel data for the fetched user
+       *
+       * @var stdClass
+       */
+      var $channel_data = null;
+      var $curl_cache;
+  
+      public function __construct($client_id, $client_secret, $redirect_url, $scope_array) {
+          $this->client_id = $client_id;
+          $this->client_secret = $client_secret;
+          $this->redirect_url = $redirect_url;
+          $this->scope_array = $scope_array;
+      }
+
     /**
      * Generates a link based on the desired scope
      *
