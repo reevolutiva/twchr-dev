81e61589319849d74196491a753dbb8832a1bffe
jerexxypunto_gear <jerexxypunto@gmail.com>
2022-11-30 09:56:36 -0300

    quito url

--- twtcher.php
+++ twtcher.php
@@ -197,9 +197,7 @@ add_action('shutdown','twchr_redirect_setUp');
 // Funcion para redurecian a pagina de instalacion
 function twchr_redirect_setUp(){
     // Si la url contiene 'plugins' retorna true
-    $dataUrl1 = str_contains($_SERVER['REQUEST_URI'],'plugins');
-    $dataUrl2 = str_contains($_SERVER['REQUEST_URI'],'action=upload-plugin');
-    $dataUrl3 = str_contains($_SERVER['REQUEST_URI'],'post_type=twchr_streams&page=twchr-dashboard');
+    $dataUrl1 = str_contains($_SERVER['REQUEST_URI'],'post_type=twchr_streams&page=twchr-dashboard');
     //show_dump(twittcher_data_exist('twchr_setInstaled'));
 
     //add_option('twchr_setInstaled',0,'',true );
@@ -208,7 +206,7 @@ function twchr_redirect_setUp(){
     //show_dump($setInstaled);
     //echo "dentro de la funcion redirect_setUp";
     if($setInstaled <= 1 || $setInstaled == false){
-        if($dataUrl1 || $dataUrl2 || $dataUrl3){  
+        if($dataUrl1){  
             $url = TWCHR_ADMIN_URL.'edit.php?post_type=twchr_streams&page=twchr_help&setUpPage=true';
             echo "<script>location.href='$url'</script>";
             //exit;
