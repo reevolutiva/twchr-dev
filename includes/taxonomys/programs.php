6f2e393ed210f758768dcfaaa6075d9fe4aa46c1
giolapietra <76657648+giolapietra@users.noreply.github.com>
2022-12-09 10:57:26 -0300

    Update programs.php
    Ordenando Taxonomía Series

--- includes/taxonomys/programs.php
+++ includes/taxonomys/programs.php
@@ -1,28 +1,45 @@
 <?php
-
-add_action( 'serie_edit_form_fields', 'twchr_edit_taxonomy_cf_to_api',10, 2 );
-
-function twchr_edit_taxonomy_cf_to_api($term,$taxonomy) {
-    //wp_nonce_field( 'serie_cf', 'serie_cf_nonce' );
-    $dateTime = get_term_meta( $term->term_id, 'twchr_toApi_dateTime', true );
-	$duration = get_term_meta( $term->term_id, 'twchr_toApi_duration', true );
-    $select_value = get_term_meta($term->term_id,'twchr_toApi_category_value',true);
-    $select_value = sanitize_text_field($select_value);
-    $select_name = get_term_meta($term->term_id,'twchr_toApi_category_name',true);
-    $select_name = sanitize_text_field($select_name);
-	$allData = get_term_meta( $term->term_id, 'twchr_fromApi_allData', true );
-
-    $select_cat = array(
-        'name' => $select_name,
-        'value' => $select_value
-    );
-    //show_dump($dateTime);
-
-	require_once 'form_programs.php';
+//Funciones End Point 
+//Haciendo visible en el Enpoint 
+  function twchr_serie_endpoint() {
+    register_rest_route( 'twchr/', 'twchr_get_serie', array(
+        'methods'  => WP_REST_Server::READABLE,
+        'callback' => 'twchr_api_get_serie',
+    ) );
 }
 
+add_action( 'rest_api_init', 'twchr_serie_endpoint' );
 
-add_action('init', 'twchr_taxonomy_serie');
+// Recopila las taxonomías y las pasa al Endpoint de Wordpress
+function twchr_api_get_serie( $request ) {
+    $args = array(
+        'taxonomy' => 'serie',
+        'hide_empty' => false
+    );
+    $request = get_terms($args);
+    $response = array();
+    foreach($request as $term){
+        $term_id = $term->{'term_id'};
+        $array_rest = array(
+            "term_id" => $term_id,
+            "name" => $term->{'name'},
+            "taxonomy" => $term->{'taxonomy'},
+            "dateTime" => get_term_meta( $term_id, 'twchr_toApi_dateTime', true ),
+            "duration" => get_term_meta( $term_id, 'twchr_toApi_duration', true ),
+            "select" => get_term_meta( $term_id, 'twchr_toApi_category', true ),
+            "dataFromTwitch" => get_term_meta( $term_id, 'twchr_fromApi_allData', true )
+        );
+
+        array_push($response, $array_rest);
+    }
+
+    return $response;
+}
+//Fin End Point
+
+//Manejadores de taxonomía Series
+
+//Registra la taxonomía Serie en Wordpress
 function twchr_taxonomy_serie() {
     $labels = array(
         'name'              => _x( 'Series', 'taxonomy general name' , 'twitcher'),
@@ -48,6 +65,7 @@ function twchr_taxonomy_serie() {
     register_taxonomy( 'serie', array( 'post', 'twchr_streams' ), $args );
 }
 
+//guarda una entrada de la taxonomía Serie en WordPress
 function twchr_taxnonomy_save( $term_id, $tt_id ) {
     
     $dateTime_old = get_term_meta( $term_id, 'twchr_toApi_dateTime', true );
@@ -55,72 +73,31 @@ function twchr_taxnonomy_save( $term_id, $tt_id ) {
     $select_old = get_term_meta( $term_id, 'twchr_toApi_category', true );
     $select_value_old = get_term_meta($term_id,'twchr_toApi_category_value',true);
     $select_name_old = get_term_meta($term_id,'twchr_toApi_category_name',true);
-    
-        
+            
     // Saneamos lo introducido por el usuario.            
     $dateTime = sanitize_text_field($_POST['twchr_toApi_dateTime']);
     $duration = sanitize_text_field($_POST['twchr_toApi_duration']);
     $select_value = sanitize_text_field($_POST['twchr_toApi_category_value']);
     $select_name = sanitize_text_field($_POST['twchr_toApi_category_name']);
-    
-        
+	
     // Actualizamos el campo meta en la base de datos.
     update_term_meta($term_id,'twchr_toApi_dateTime',$dateTime,$dateTime_old);
     update_term_meta($term_id,'twchr_toApi_duration',$duration, $duration_old);
     update_term_meta($term_id,'twchr_toApi_category_value',$select_value, $select_value_old);
     update_term_meta($term_id,'twchr_toApi_category_name',$select_name, $select_name_old);
-    
-
-    
     if(isset($_POST['twchr_toApi_dateTime']) && isset($_POST['twchr_toApi_duration']) && isset($_POST['twchr_toApi_category_value']) ){
             $response = twchr_serie_update($term_id);
-            $allData = json_encode($response);
-            
+            $allData = json_encode($response);          
             update_term_meta($term_id,'twchr_fromApi_allData',$allData);
-            
-            
-            
-        
    }
-   
   }
-  add_action( 'edit_serie', 'twchr_taxnonomy_save', 10,5);
+add_action('init', 'twchr_taxonomy_serie'); //Fin Guardar taxonomía
 
+add_action( 'edit_serie', 'twchr_taxnonomy_save', 10,5);
 
-  function twchr_serie_endpoint() {
-    register_rest_route( 'twchr/', 'twchr_get_serie', array(
-        'methods'  => WP_REST_Server::READABLE,
-        'callback' => 'twchr_api_get_serie',
-    ) );
-}
-
-add_action( 'rest_api_init', 'twchr_serie_endpoint' );
-
-function twchr_api_get_serie( $request ) {
-    $args = array(
-        'taxonomy' => 'serie',
-        'hide_empty' => false
-    );
-    $request = get_terms($args);
-    $response = array();
-    foreach($request as $term){
-        $term_id = $term->{'term_id'};
-        $array_rest = array(
-            "term_id" => $term_id,
-            "name" => $term->{'name'},
-            "taxonomy" => $term->{'taxonomy'},
-            "dateTime" => get_term_meta( $term_id, 'twchr_toApi_dateTime', true ),
-            "duration" => get_term_meta( $term_id, 'twchr_toApi_duration', true ),
-            "select" => get_term_meta( $term_id, 'twchr_toApi_category', true ),
-            "dataFromTwitch" => get_term_meta( $term_id, 'twchr_fromApi_allData', true )
-        );
-
-        array_push($response, $array_rest);
-    }
-
-    return $response;
-}
 add_action( 'create_serie', 'twchr_save_serie_redirect', 10,5);
+
+//Redirecciona del create al Edit de la Taxonomía
 function twchr_save_serie_redirect($term_id, $tt_id){
    ?>
     <script>
@@ -133,14 +110,13 @@ function twchr_save_serie_redirect($term_id, $tt_id){
    die();
 }
 
-
+//Actualiza  
 function twchr_serie_update($term_id) {
 
     // Recoje data de BDD
     $twch_data_prime = json_decode(get_option( 'twchr_keys', false ));
     $tokenValidate = $twch_data_prime->{'user_token'};
     $client_id = $twch_data_prime->{'client-id'};
-
 
     $dateTime_raw = sanitize_text_field($_POST['twchr_toApi_dateTime']);
     $dateTime_stg = strtotime($dateTime_raw);
@@ -170,6 +146,25 @@ function twchr_serie_update($term_id) {
     }
     // Envia los datos a la API de twich
     return twchr_post_stream($term_id,$tokenValidate,$client_id,$tag_name,$dateTime_rfc ,$select_value,$duration);
-
-    
 }
+
+//Formulario que aparece en el Edit de la taxonomía Series en Wordpress
+function twchr_edit_taxonomy_cf_to_api($term,$taxonomy) {
+    //wp_nonce_field( 'serie_cf', 'serie_cf_nonce' );
+    $dateTime = get_term_meta( $term->term_id, 'twchr_toApi_dateTime', true );
+	$duration = get_term_meta( $term->term_id, 'twchr_toApi_duration', true );
+    $select_value = get_term_meta($term->term_id,'twchr_toApi_category_value',true);
+    $select_value = sanitize_text_field($select_value);
+    $select_name = get_term_meta($term->term_id,'twchr_toApi_category_name',true);
+    $select_name = sanitize_text_field($select_name);
+	$allData = get_term_meta( $term->term_id, 'twchr_fromApi_allData', true );
+
+    $select_cat = array(
+        'name' => $select_name,
+        'value' => $select_value
+    );
+    //show_dump($dateTime);
+
+	require_once 'form_programs.php';
+}
+add_action( 'serie_edit_form_fields', 'twchr_edit_taxonomy_cf_to_api',10, 2 );
