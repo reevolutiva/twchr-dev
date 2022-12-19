4feeb02b4949f2fb9ff6b83f98a4c3bfc29ecfb3
jerexxypunto_gear <jerexxypunto@gmail.com>
2022-12-19 10:33:54 -0300

    quito custom field inesesario

--- includes/cf/streaming_custom_tab3.php
+++ includes/cf/streaming_custom_tab3.php
@@ -1,8 +1,6 @@
 <?php 
 $values    = get_post_custom( $_GET['post'] );
-//show_dump($values);
-$dateTime = isset($values['twchr_stream_data_dateTime'][0]) ? $values['twchr_stream_data_dateTime'][0] : '';
-$dateTimeTwitch = isset($values['twchr-from-api_create_at'][0]) ? $values['twchr-from-api_create_at'][0] : false;
+
 $yt_url = get_post_meta( get_the_ID(), 'twchr_streams__yt-link-video-src', true );
 $select = isset($values['twchr_stream_src_priority']) ? $values['twchr_stream_src_priority'][0] : ''; 
 ?>
@@ -10,11 +8,7 @@ $select = isset($values['twchr_stream_src_priority']) ? $values['twchr_stream_sr
     <picture>
         <img src="<?php echo TWCHR_URL_ASSETS.'logo_menu.svg' ?>" alt="logo-twitch">
     </picture>
-    <label>Fecha y hora del streming 
-        <input <?php echo $dateTimeTwitch == false ? null : 'disabled'?>
-            type="<?php echo $dateTimeTwitch == false ? "datetime-local" : "text"?>" name='twchr_stream_data_dateTime'
-            value="<?php if($dateTimeTwitch){ twchr_esc_i18n($dateTimeTwitch,'html'); }else{ twchr_esc_i18n($dateTime,'html');}  ?>">
-    </label>
+    <label>Fecha y hora del streming:  <?php echo $dateTime; ?> </label>
     <label><?php twchr_esc_i18n('Source Priority','html'); ?></label>
     <select name="twchr_stream_src_priority">
         <option value="tw" <?php selected($select,'tw')?>>Twitch</option>
