1b4ff6b3b098a91cc1461f6a908a1a676889ebd8
jerexxypunto_gear <jerexxypunto@gmail.com>
2022-12-23 09:29:58 -0300

    estandariso ext-wp-api

--- includes/ext-wp-api.php
+++ includes/ext-wp-api.php
@@ -1,16 +1,22 @@
 <?php
-// Haciendo visible en el Enpoint
-// Taxonomía Series
-// twchr_endpoint_tax
-// twchr_endpoint_tax_register_serie
-function twchr_endpoint_tax_register_serie() {
+/**
+ * En este archivo esta todas las funciones que extienden la API REST de WordPress
+ */
+
+/**
+ * Haciendo visible en el Enpoint
+ * Taxonomía Series
+ * twchr_endpoint_tax
+ * twchr_endpoint_tax_register_serie
+**/
+ function twchr_endpoint_tax_register_serie() {
 	register_rest_route(
 		'twchr/v1',
 		'twchr_get_serie',
 		array(
 			'methods'  => WP_REST_Server::READABLE,
 			'callback' => 'twchr_endpoint_tax_register_callback_serie',
-		/*
+		/** 
 		'permission_callback' => function(){
 			return current_user_can( 'read' );
 		}
@@ -21,8 +27,12 @@ function twchr_endpoint_tax_register_serie() {
 }
 add_action( 'rest_api_init', 'twchr_endpoint_tax_register_serie' );// twchr_endpoint_tax_register_serie
 
-// twchr_endpoint_tax_register_callback_serie
-// Recopila las taxonomías y las pasa al Endpoint de Wordpress
+/**
+ * Recopila las taxonomías y las pasa al Endpoint de Wordpress
+ *
+ * @param [type] $request
+ * @return void
+ */
 function twchr_endpoint_tax_register_callback_serie( $request ) {
 	$args = array(
 		'taxonomy' => 'serie',
@@ -44,10 +54,15 @@ function twchr_endpoint_tax_register_callback_serie( $request ) {
 
 	return $response;
 }
-// Fin Taxonomía Series //fin twchr_endpoint_tax_register_callback_serie
+/**
+ *  Fin Taxonomía Series 
+ */
 
-// CPT Streamings
-// twchr_endpoint_cpt_register_streaming
+/**
+ * Hago visible a los CPT Streamins en la API REST de WordPress
+ *
+ * @return void
+ */
 function twchr_endpoint_cpt_register_streaming() {
 	register_rest_route(
 		'twchr/v1',
@@ -55,7 +70,7 @@ function twchr_endpoint_cpt_register_streaming() {
 		array(
 			'methods'  => 'GET',
 			'callback' => 'twchr_endpoint_cpt_register_callback_streaming',
-		/*
+		/**
 		'permission_callback' => function(){
 			return current_user_can( 'read' );
 		}
@@ -65,9 +80,14 @@ function twchr_endpoint_cpt_register_streaming() {
 }
 add_action( 'rest_api_init', 'twchr_endpoint_cpt_register_streaming' );
 
-// twchr_endpoint_cpt_register_callback_streaming
+/**
+ * Recopilo los CPT Streamings
+ *
+ * @param [type] $request
+ * @return void
+ */
 function twchr_endpoint_cpt_register_callback_streaming( $request ) {
-	// Solicita a BDD todos los post-type = twchr_streams que esten plubicados
+	// Solicita a BDD todos los post-type = twchr_streams que esten plubicados.
 	$posts = get_posts(
 		array(
 			'post_type'  => 'twchr_streams',
@@ -75,35 +95,39 @@ function twchr_endpoint_cpt_register_callback_streaming( $request ) {
 		)
 	);
 
-	// Inicializo un array vacio
+	// Inicializo un array vacio.
 	$array_response = array();
 
-	// Itero la List post-type
+	// Itero la List post-type.
 	foreach ( $posts as $key => $value ) {
 			$id = $value->{'ID'}; // guardo su id
-			$title = $value->{'post_title'}; // guardo su title
-			$stream_id = get_post_meta( $id, 'twchr-from-api_id', true ); // guardo el custom-field steram_id
+			$title = $value->{'post_title'}; // guardo su title.
+			$stream_id = get_post_meta( $id, 'twchr-from-api_id', true ); // guardo el custom-field steram_id.
 
-			// Guardo los datos anteriores en un array
+			// Guardo los datos anteriores en un array.
 			$post_for_api = array(
 				'wordpress_id' => $id,
 				'title' => $title,
-				'twchr_id' => (int) $stream_id, // Convierto stream_id a numero entero
+				'twchr_id' => (int) $stream_id, // Convierto stream_id a numero entero.
 
 			);
 
-			// guardo $post_for_api en array_response
+			// guardo $post_for_api en array_response.
 			array_push( $array_response, $post_for_api );
 	}
-	// retorno array_response
+	// retorno array_response.
 	return $array_response;
 
-} //twchr_endpoint_cpt_register_callback_streaming fin
+} //twchr_endpoint_cpt_register_callback_streaming fin.
 
-// Fin cpt streamings
+// Fin cpt streamings.
 
 
-
+/**
+ * Hago visibles las categorias de twich en la API REST de wordpress
+ *
+ * @return void
+ */
 function twchr_cat_twcht_endpoint() {
 	register_rest_route(
 		'twchr/v1',
