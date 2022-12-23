75c392a824ab43d4aa8f03783422409685885787
jerexxypunto_gear <jerexxypunto@gmail.com>
2022-12-23 16:35:24 -0300

    cpt update host dinamico

--- includes/update-post/update-twchr-stream.php
+++ includes/update-post/update-twchr-stream.php
@@ -23,12 +23,19 @@
  * @return void
  */
 function twitcher_update_cpt( $title, $id, $create_at, $description, $duration, $api_languaje, $muted_segment, $published_at, $stream_id, $thumbnail_url, $type, $url, $user_id, $user_login, $user_name, $view_count, $viewble, $author = 1 ) {
+	$display_name = '';
+	$data_broadcaster_raw = get_option( 'twchr_data_broadcaster', false ) == false ? false : json_decode( get_option( 'twchr_data_broadcaster' ) );
+	if ( $data_broadcaster_raw != false ) {
+		$display_name = $data_broadcaster_raw->{'data'}[0]->{'display_name'};
+	}
+		
+
 	$postarr = array(
 		'post_title' => $title,
 		'post_status' => 'publish',
 		'post_author' => $author,
 		'post_type' => 'twchr_streams',
-		'post_content' => '[twich_embed host="reevolutiva" video="' . $id . '"  ancho="800" alto="400"]',
+		'post_content' => '[twchr_tw_video host="'.$display_name.'" video="' . $id . '"  ancho="800" alto="400"]',
 		'meta_input'   => array(
 			'twchr-from-api_create_at' => $create_at,
 			'twchr-from-api_description' => $description,
