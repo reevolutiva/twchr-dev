601a3b1b8f6b99334327a4026d885a91cb059b62
jerexxypunto_gear <jerexxypunto@gmail.com>
2022-12-26 08:38:02 -0300

    funcionalidad de clipboard integrada

--- includes/assets/plugin-enqueue.php
+++ includes/assets/plugin-enqueue.php
@@ -45,8 +45,6 @@ function twchr_admin_js() {
 
 	// Scripts.
 	wp_enqueue_script( 'twchr_gscjs', TWCHR_URL . 'includes/js/gscjs.js', array(), $version, false );
-	wp_enqueue_script( 'twchr_clipboard', 'https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/2.0.8/clipboard.min.js', array(), $version, false );
-	
 
 	$set_instaled = get_option( 'twchr_set_instaled' );
 	// Usamos esta función para que coloque los valores inline
@@ -68,3 +66,9 @@ function twchr_admin_js() {
 }
 
 add_action( 'admin_enqueue_scripts', 'twchr_admin_js' );
+
+function twchr_front_scripts(){
+	wp_enqueue_script( 'twchr_clipboard-cdn', 'https://cdnjs.cloudflare.com/ajax/libs/clipboard.js/2.0.8/clipboard.min.js', array(), $version, false );
+}
+
+add_action('wp_enqueue_scripts', 'twchr_front_scripts');
--- includes/features/twchr_clipboard.php
+++ includes/features/twchr_clipboard.php
@@ -3,13 +3,13 @@ function twchr_clipboard_include(){ ?>
 
     <script>
     const twchr_btns = document.querySelectorAll('button.twchr_clipboard');
-    const twcjr_clipboard = new ClipboardJS(btns);
+    const twcjr_clipboard = new ClipboardJS(twchr_btns);
 
-    clipboard.on('success', function(e) {
+    twcjr_clipboard.on('success', function(e) {
         console.log(e);
     });
 
-    clipboard.on('error', function(e) {
+    twcjr_clipboard.on('error', function(e) {
         console.log(e);
     });
 </script>
