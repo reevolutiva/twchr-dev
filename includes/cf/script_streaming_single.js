d6215ea63a8e44b0d37e97dcb8e3db7314f0c711
jerexxypunto_gear <jerexxypunto@gmail.com>
2023-01-17 12:45:49 -0300

    solucionado en lcoal

--- includes/cf/script_streaming_single.js
+++ includes/cf/script_streaming_single.js
@@ -380,7 +380,7 @@ twchr_modal_schedule__btn.addEventListener('click',e => {
               }
             );
             setTimeout(() => {
-              //location.href = twchr_admin_url+"post.php?post="+e.post_id+"&action=edit";
+              location.reload();
             }, 1000);
           }else{
             console.log(e);
